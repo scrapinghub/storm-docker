4987b4456b936be9c07c95328b479cb3a2cf121e
Viktor Shlapakov <vshlapakov@gmail.com>
2015-04-01 17:41:41 +0300

    Updated dockerfiles & cfgs using kafka-storm-docker

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # FIXME should be replaced with scrapinghub account
-docker build -t="vshlapakov/storm" storm
+docker build -t="vshlapakov/storm-base:0.9.4" storm
 docker build -t="vshlapakov/storm-nimbus" storm-nimbus
 docker build -t="vshlapakov/storm-supervisor" storm-supervisor
 docker build -t="vshlapakov/storm-ui" storm-ui
