4d84bd9228c5a0804a8a9e672aff8b05d43b624c
Viktor Shlapakov <vshlapakov@gmail.com>
2015-04-03 18:51:23 +0300

    Simplified Zookeeper IP using

--- start-storm.sh
+++ start-storm.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-# Define Zookeeper servers
-ZKINST=$(($ZK_INSTANCES))
-if [[ ZKINST -gt 0 ]]; then
-  for i in `seq 1 $ZKINST`; do
-    ZK=$'ZK'$i$'_PORT_2181_TCP_ADDR'
-    if [[ -z "$ZKADDR" ]]; then
-      [[ ! -z "${!ZK}" ]] && ZKADDR=${!ZK}
-    else
-      [[ ! -z "${!ZK}" ]] && ZKADDR=$ZKADDR$','${!ZK}
-    fi
-  done
-else
-  ZKADDR=$ZK_PORT_2181_TCP_ADDR
-fi
-
 # Define Nimbus server
 NIMBUS_IP=`hostname -i`
 [[ ! -z "$NIMBUS_PORT_6627_TCP_ADDR" ]] && NIMBUS_IP=$NIMBUS_PORT_6627_TCP_ADDR
 
 # Update Storm config file
-sed -i -e "s/%zookeeper%/$ZKADDR/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$NIMBUS_IP/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
