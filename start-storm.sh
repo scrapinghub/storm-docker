29dfa10bd132ec9230b5a2b57e2666dcbeec0a33
Viktor Shlapakov <vshlapakov@gmail.com>
2015-04-06 20:59:15 +0300

    Using dynamic DRPC ports

--- start-storm.sh
+++ start-storm.sh
@@ -6,11 +6,19 @@ NIMBUS_IP=`hostname -i`
 NUMBUS_PORT="6628"
 [[ ! -z "$NIMBUS_PORT_6628_TCP_PORT" ]] && NIMBUS_PORT=$NIMBUS_PORT_6628_TCP_PORT
 
+# Define DRPC ports
+DRPC_PORT="3774"
+[[ ! -z "$DRPC_PORT_3774_TCP_PORT" ]] && DRPC_PORT=$DRPC_PORT_3774_TCP_PORT
+DRPC_INVOCATIONS_PORT="3775"
+[[ ! -z "$DRPC_INVOCATIONS_PORT_3775_TCP_PORT" ]] && DRPC_INVOCATIONS_PORT=$DRPC_INVOCATIONS_PORT_3775_TCP_PORT
 
 # Update Storm config file
 sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$NIMBUS_IP/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus-port%/$NIMBUS_PORT/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%drpc-port%/$DRPC_PORT/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%drpc-invocations-port%/$DRPC_INVOCATIONS_PORT/g" $STORM_HOME/conf/storm.yaml
+
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
 
